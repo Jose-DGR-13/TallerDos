2ba508f4d0e3e00740acb1ce21a99b45fcd79a42
JoseD <JoseD@172.30.169.206>
2016-09-16 15:22:41 -0500

    Clases iniciales

---
+++ workspace/DCA_SemanaSiete/src/Archivo.java
@@ -0,0 +1,11 @@
+import processing.core.PApplet;
+
+public class Archivo {
+	
+	PApplet app = MainApp.app;
+	
+	public Archivo() {
+		// TODO Auto-generated constructor stub
+	}
+	
+}
---
+++ workspace/DCA_SemanaSiete/src/Logica.java
@@ -0,0 +1,10 @@
+import processing.core.PApplet;
+
+public class Logica {
+	
+	PApplet app = MainApp.app;
+	
+	public Logica() {
+	}
+	
+}
